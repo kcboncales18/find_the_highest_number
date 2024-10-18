30a1226ca1766add5bc7589e92a11d9def545a92
kcboncales.18 <kirstencaryl.l.boncales@gmail.com>
2024-10-18 23:19:24 +0800

    compare the variables

--- finding_the_highest_number.py
+++ finding_the_highest_number.py
@@ -2,4 +2,24 @@
 
 #define_the_variables
 def find_highest (num_1, num_2, num_3, num_4, num_5):
+
+#compare_the_variables
+    if num_1 > num_2 and num_3:
+        if num_1 > num_4 and num_5:
+            return num_1
     
+    if num_2 > num_1 and num_3:
+        if num_2 > num_4 and num_5:
+            return num_2
+    
+    if num_3 > num_1 and num_2:
+        if num_3 > num_4 and num_5:
+            return num_3
+    
+    if num_4 > num_1 and num_2:
+        if num_4 > num_3 and num_5:
+            return num_4
+    
+    if num_5 > num_1 and num_2:
+        if num_5 > num_3 and num_4:
+            return num_5
